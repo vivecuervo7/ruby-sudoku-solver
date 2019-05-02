563c4db2c848e9847b951d974f0320b050c50c79
vivecuervo7 <vivecuervo7@gmail.com>
2019-05-03 08:33:14 +1000

    Cleanup and added comments where necessary

--- src/classes/sudoku_board.rb
+++ src/classes/sudoku_board.rb
@@ -6,10 +6,10 @@ class SudokuBoard
 
     def instantiate_board
         board = []
+        # Populate board with array of sudoku_cell of size 81, default values of zero
         for row in (0..8) do
             for col in (0..8) do
-                default_value = 0
-                board << SudokuCell.new(row, col, default_value)
+                board << SudokuCell.new(row, col, 0)
             end
         end
         return board
@@ -18,65 +18,49 @@ class SudokuBoard
     def populate_board
         i = 0;
         while i < 81 do
-            if @cells[i] == nil
-                puts "Cell is Nil at index #{i}"
-            end
-            #puts "\nChecking coordinates [#{@cells[i].x},#{@cells[i].y}]"# - following options available #{options}" # Testing
+            # Try to assign a value to current cell, increment i if operation was a success
+            # identify_neighbors here passes current cell, returns array of neighboring cells
+            # assign_value passes the array of neighbors mapped so as to only pass an array of the neighbors' values
             if @cells[i].assign_value(identify_neighbors(@cells[i]).map { |c| c.value.to_i })
                 i += 1
             else
+                # Reset current cell to zero, reset that cell's options
+                # Also reset used_options for the previously assessed cell to cover the case of consecutive backtracks
                 @cells[i].reset()
                 @cells[i+1].reset(true)
                 i -= 1
             end
         end
-
-        # @cells.shuffle.each { |cell|
-        #     options = [1,2,3,4,5,6,7,8,9]
-        #     identify_neighbors(cell).each { |neighbor|
-        #         if !neighbor.equals?(cell)
-        #             options.delete(neighbor.value)
-        #         end
-        #     }
-
-        #     puts "\nChecking coordinates [#{cell.x},#{cell.y}] - following options available #{options}" # Testing
-
-        #     #if options.length > 0
-        #         cell.value = options.shuffle[0]
-        #     #end
-        # }
     end
 
     def identify_neighbors(cell)
         neighbors = []
         
-        # Get all cells in row and column
+        # Select all cells in row and column, excluding self and cells with values of zero
         for i in 0..8 do
-            cells_checked = @cells.select { |c| ((c.x == cell.x && c.y == i) || (c.x == i && c.y == cell.y)) && c != nil && c.value.to_i > 0 }
+            cells_checked = @cells.select { |c| ((c.x == cell.x && c.y == i) || (c.x == i && c.y == cell.y)) && c != cell && c.value.to_i > 0 }
             if cells_checked.length > 0
                 neighbors << cells_checked[0]
             end
         end
 
-        # Get all cells in block
+        # Select all cells in current block of nine cells, excluding self and cells with values of zero
         col_block_index = (cell.x / 3).to_i * 3
         row_block_index = (cell.y / 3).to_i * 3
         for i in col_block_index..col_block_index + 2 do
             for j in row_block_index..row_block_index + 2 do
-                cells_checked = @cells.select { |c| c.x == i && c.y == j && c.value.to_i > 0 }
+                cells_checked = @cells.select { |c| c.x == i && c.y == j && c != cell && c.value.to_i > 0 }
                 if cells_checked.length > 0
                     neighbors << cells_checked[0]
                 end
             end
         end
-
-        # Remove self from neighbors
-        neighbors.delete(cell)
         
         return neighbors
     end
 
     def format_as_two_dimensional_array
+        # Return @cells formatted as an array, necessary for table rendering
         board = Array.new(9) { Array.new(9) }
         @cells.each { |cell|
             board[cell.x][cell.y] = cell.value
--- src/classes/sudoku_cell.rb
+++ src/classes/sudoku_cell.rb
@@ -1,38 +1,40 @@
 class SudokuCell
-    attr_accessor :value
-    attr_reader :x, :y#, :value # Swap this back to attr_reader
+    attr_reader :x, :y, :value
 
     def initialize(x, y, value)
         @x = x
         @y = y
         @value = value
-        @options = [1,2,3,4,5,6,7,8,9]
+        @options = (1..9).to_a
         @used_options = []
     end
 
     def assign_value(unavailable_options)
+
+        # Remove unavailable options due to used or neighbor cell
         @options -= unavailable_options
         @options -= @used_options
+
+        # Assign a value if options are greater than zero, and add that selection to used_options
         if @options.length > 0
             @value = @options.shuffle.shift
             @used_options[@used_options.length] = @value
-            #puts "Success\t\t(#{@x},#{@y}),\tunavailable #{unavailable_options}, remaining #{@options}, used#{@used_options}, assigned #{value}"
             puts "   \t(#{@x},#{@y})\tusing value [#{@value}] from options #{@options}"
             return true
         end
+
+        # Otherwise, reset value to zero and return false, so sudoku_board will backtrack through previous options
         puts "ERR\t(#{@x},#{@y})\treset value [ ] backtrack"
         reset
-        #puts "Backtrack\t(#{@x},#{@y}),\tunavailable #{unavailable_options}, remaining #{@options}, used#{@used_options}, reset to #{value}"
         return false
     end
 
-    def reset(hard = false)
+    def reset(hard_reset = false)
+        # Reset value and options, if hard_reset due to consecutive backtracks, also reset used_options
         @value = 0
-        @options = [1,2,3,4,5,6,7,8,9]
-        @used_options = [] if hard
-    end
-
-    def equals?(other)
-        return @x == other.x && @y == other.y
+        @options = (1..9).to_a
+        if hard_reset
+            @used_options.clear
+        end
     end
 end
