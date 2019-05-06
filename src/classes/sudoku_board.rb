cf62401906f4a1f5582dbabac60a25128a710c97
vivecuervo7 <vivecuervo7@gmail.com>
2019-05-07 09:21:37 +1000

    Fixed non-completion issue, removed sorting by number of options, was detrimental due to the randomness of the cells - made later stages of the puzzle take a lot longer than iterating by each cell

--- src/classes/sudoku_board.rb
+++ src/classes/sudoku_board.rb
@@ -2,7 +2,7 @@ class SudokuBoard
     def initialize
         @cells = instantiate_board
         @test_puzzle = "010020300004005060070000008006900070000100002030048000500006040000800106008000000"
-        @test_puzzle_answer = "815627394924385761673491528186952473457163982239748615591236847342879156768514239"
+        @test_puzzle_answer = "815627394924385761673491528186952473457463982239748615591236847342879156768514239"
         populate_board(@test_puzzle)
     end
 
@@ -28,50 +28,36 @@ class SudokuBoard
             end
         end
 
-        # Pre-calculate all current cell's options
-        for i in 0..@cells.length - 1 do
-            @cells[i].calculate_options(identify_neighbors(@cells[i]).map { |c| c.value.to_i })
-        end
-
-        # Sort so that program will assess those with the fewest options first in an attempt to limit backtracking
-        @cells.sort_by! { |c| c.options.length }.reverse
-
         i = 0
         direction = 1
         while i < @cells.length do
             # Try to assign a value to current cell, increment i if operation was a success
             # identify_neighbors here passes current cell, returns array of neighboring cells
             # assign_value passes the array of neighbors mapped so as to only pass an array of the neighbors' values
-            if @cells[i].predefined
-                # If cell was predefined ignore value assignment, hard reset the previous cell if backtracking
-                #if direction == -1
-                    @cells[i + 1].reset(true)
-                #end
-            elsif @cells[i].assign_value(identify_neighbors(@cells[i]).map { |c| c.value.to_i })
-                direction = 1
-                # Testing output
-                render_progress
-            else
-                # Reset current cell to zero, reset that cell's options
-                # Also reset used_options for the previously assessed cell to cover the case of consecutive backtracks
-                @cells[i].reset()
-                @cells[i + 1].reset(true)
-                direction = -1
-                # Testing output
-                render_progress
+            if !@cells[i].predefined
+                if @cells[i].assign_value(identify_neighbors(@cells[i]).map { |c| c.value.to_i })
+                    direction = 1
+                    render_progress
+                else
+                    # Reset current cell to zero, reset that cell's options
+                    # Also reset used_options for the previously assessed cell to cover the case of consecutive backtracks
+                    @cells[i].reset()
+                    direction = -1
+                    render_progress
+                end
             end
+            @cells[i + 1].reset(true) if i < 80
             i += direction
         end
     end
 
     def render_progress
-        # TESTING OUTPUT ONLY
         table = TTY::Table.new rows: format_as_two_dimensional_array
         renderer = TTY::Table::Renderer::Unicode.new(table)
         renderer.padding = [0,1]
         renderer.border.separator = :each_row
         puts renderer.render
-        sleep 0.1
+        #sleep 0.1
     end
 
     def identify_neighbors(cell)
@@ -99,7 +85,7 @@ class SudokuBoard
         show_correct = true
         board = Array.new(9) { Array.new(9) }
         @cells.each { |cell|
-            correct = cell.value == @test_puzzle_answer[cell.x + cell.y].to_i
+            correct = cell.value == @test_puzzle_answer[@cells.index(cell)].to_i
             if show_correct
                 board[cell.x][cell.y] = cell.value > 0 ? cell.predefined ? "\e[36m#{cell.value}\e[0m" : correct ? "\e[32m#{cell.value}\e[0m" : "\e[31m#{cell.value}\e[0m" : " "
             else
